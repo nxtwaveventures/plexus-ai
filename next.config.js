417e8d4990ab3831c0eccb8aecd1eb0764216b7e
Your Name <your.email@example.com>
2025-05-02 14:29:18 +0530

    fix: Remove revalidate export from BAA page

--- next.config.js
+++ next.config.js
@@ -11,22 +11,6 @@ const nextConfig = {
     },
     basePath: '',
     assetPrefix: '',
-    exportPathMap: async function () {
-        return {
-            '/': { page: '/' },
-            '/plexus-lifex': { page: '/plexus-lifex' },
-            '/plexus-lifex/baa': { page: '/plexus-lifex/baa' },
-            '/plexus-lifex/baa/': { page: '/plexus-lifex/baa' },
-        };
-    },
-    rewrites: async () => {
-        return [
-            {
-                source: '/plexus-lifex/baa',
-                destination: '/plexus-lifex/baa/',
-            },
-        ];
-    }
-};
+}
 
 module.exports = nextConfig; 
