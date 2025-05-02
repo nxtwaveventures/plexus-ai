348b6d92a7daf376b030d5e6678c3275ac8707b1
nxtwaveventures <nxtwave.ventures@gmail.com>
2025-05-02 08:28:48 +0000

    Deploying to gh-pages from @ nxtwaveventures/plexus-ai@379d53f97669a18c3629fa0cedcca9f768bd0b7a 🚀

--- _next/static/DdDvqb3ti6x0a2bJ7SaWz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[{has:void 0,source:"/plexus-lifex/baa",destination:"/plexus-lifex/baa/index.html"}],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-28b803cb2479b966.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
